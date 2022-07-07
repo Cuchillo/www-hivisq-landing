4545c6adc4ce32ba9df3cbcc965431d9dda6214d
Jose Sentis <jose.sentis1987@gmail.com>
2022-07-07 14:52:58 +0200

    WIP: metas

--- src/_data/_site.js
+++ src/_data/_site.js
@@ -2,15 +2,15 @@ const timestamp = new Date();
 
 module.exports = {
   lang: "es",
-  name: "WDC Valencia 2022",
-  title: "WDC Valencia 2022",
-  description: "Wordpress + 11ty + Cuchillo Core",
+  name: "Canteo | Challenging the Future of Architecture and Design",
+  title: "Canteo | Challenging the Future of Architecture and Design",
+  description: "Canteo es un colectivo creativo independiente formado por los estudios de diseño y arquitectura de Cuchillo e Imajik respectivamente, con el objetivo crear espacios híbridos únicos y exclusivos. Nuestro compromiso con las ideas y los proyectos a medida determina nuestra visión del diseño integral como lugar de convergencia natural para las disciplinas de la arquitectura, el diseño gráfico y multimedia, el branding o la producción de eventos. Desde esta diversidad podemos afrontar cada proyecto de forma ágil, versátil y flexible, desde la fase de generación de las ideas hasta hacerlas realidad.",
   type: "website",
   image: "/assets/social/facebook-1200x630.jpg",
   image_twitter: "/assets/social/twitter-600x330.jpg",
   google_analytics_code: "xxxxx",
   /* URL FRONT */
-  url: "https://www.mrcorrales.com",
+  url: "https://www.canteo.xyz",
   /* URL BACK */
   dataUrl: "",
 };
