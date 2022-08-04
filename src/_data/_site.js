46f9cb907f5ded9257c34942271f7a35daf35f5f
naiarasomoscuchillo <naiara@somoscuchillo.com>
2022-08-04 15:52:08 +0200

    desktop version

--- src/_data/_site.js
+++ src/_data/_site.js
@@ -2,15 +2,15 @@ const timestamp = new Date();
 
 module.exports = {
   lang: "es",
-  name: "Canteo | Challenging the Future of Architecture and Design",
-  title: "Canteo | Challenging the Future of Architecture and Design",
-  description: "Canteo es un colectivo creativo independiente formado por los estudios de diseño y arquitectura de Cuchillo e Imajik respectivamente, con el objetivo crear espacios híbridos únicos y exclusivos. Nuestro compromiso con las ideas y los proyectos a medida determina nuestra visión del diseño integral como lugar de convergencia natural para las disciplinas de la arquitectura, el diseño gráfico y multimedia, el branding o la producción de eventos. Desde esta diversidad podemos afrontar cada proyecto de forma ágil, versátil y flexible, desde la fase de generación de las ideas hasta hacerlas realidad.",
+  name: "Hivisq | smart views",
+  title: "Hivisq | smart views",
+  description: "Hivisq",
   type: "website",
   image: "/assets/social/facebook-1200x630.jpg",
   image_twitter: "/assets/social/twitter-600x330.jpg",
   google_analytics_code: "xxxxx",
   /* URL FRONT */
-  url: "https://www.canteo.xyz",
+  url: "https://www.hivisq.xyz",
   /* URL BACK */
   dataUrl: "",
 };
